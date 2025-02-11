96508930b0b289326ed00c4ab1ca4482db3eb01e
abdur-razzak-rouzex <abdurrazzak1663@gmail.com>
2025-02-11 19:36:46 +0600

    the dice-rolling-game implementation is complete

--- dice-rolling-game.py
+++ dice-rolling-game.py
@@ -1,12 +1,13 @@
 import random
-from queue import PriorityQueue
 
-choice = input("Roll the dice? (y/n): ").lower()
+while True:
+    choice = input("Roll the dice? (y/n): ").lower()
 
-if choice == 'y':
-    number = random.randint(1,6)
-    print(f"The dice value is: {number}")
-elif choice == 'n' :
-    print("Thank you for playing")
-else:
-    print("Invalid input")
+    if choice == 'y':
+        number = random.randint(1,6)
+        print(f"The dice value is: {number}")
+    elif choice == 'n' :
+        print("Thank you for playing")
+        break
+    else:
+        print("Invalid input")
